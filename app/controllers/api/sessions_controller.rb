03fe9fab28e99cc5a29e1183b5746cb739e32039
App Academy Student <aa@Apps-Mac-mini-4.local>
2019-03-28 18:33:03 -0400

    fixed login errors issue

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::SessionsController < ApplicationController
             login!(@user)
             render "api/users/show"
         else
-            render json: ["Invalid email/password"]
+            render json: ["Invalid email/password"], status: 403
         end
     end
 
