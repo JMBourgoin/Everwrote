38128aeb0fd532acd1b86a8ac5a5b7f326c31131
App Academy Student <aa@Apps-Mac-mini-6.local>
2019-04-02 18:31:40 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,16 +41,16 @@ notebooks = Notebook.create([
 ])
 
 notes - Note.create([
-  {title: 'Wonderful Note.', body: 'This note is about being a wonderful note.  It is a special note, this note.  Wonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Bonderful Note.', body: 'This note is about being a Bonderful note.  It is a special note, this note.  Bonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Vonderful Note.', body: 'This note is about being a Vonderful note.  It is a special note, this note.  Vonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Gonderful Note.', body: 'This note is about being a wonderful note.  It is a special note, this note.  Wonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Sonderful Note.', body: 'This note is about being a Sonderful note.  It is a special note, this note.  Sonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Xonderful Note.', body: 'This note is about being a Xonderful note.  It is a special note, this note.  Xonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Zonderful Note.', body: 'This note is about being a Zonderful note.  It is a special note, this note.  Zonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Qonderful Note.', body: 'This note is about being a Qonderful note.  It is a special note, this note.  Qonderful.', author_id: 0, notebook_id: 1}
-  {title: 'Londerful Note.', body: 'This note is about being a Londerful note.  It is a special note, this note.  Londerful.', author_id: 0, notebook_id: 1}
-  {title: 'Ponderful Note.', body: 'This note is about being a Ponderful note.  It is a special note, this note.  Ponderful.', author_id: 0, notebook_id: 1}
+  {title: 'Wonderful Note.', body: 'This note is about being a wonderful note.  It is a special note, this note.  Wonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Bonderful Note.', body: 'This note is about being a Bonderful note.  It is a special note, this note.  Bonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Vonderful Note.', body: 'This note is about being a Vonderful note.  It is a special note, this note.  Vonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Gonderful Note.', body: 'This note is about being a wonderful note.  It is a special note, this note.  Wonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Sonderful Note.', body: 'This note is about being a Sonderful note.  It is a special note, this note.  Sonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Xonderful Note.', body: 'This note is about being a Xonderful note.  It is a special note, this note.  Xonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Zonderful Note.', body: 'This note is about being a Zonderful note.  It is a special note, this note.  Zonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Qonderful Note.', body: 'This note is about being a Qonderful note.  It is a special note, this note.  Qonderful.', author_id: 0, notebook_id: 1},
+  {title: 'Londerful Note.', body: 'This note is about being a Londerful note.  It is a special note, this note.  Londerful.', author_id: 0, notebook_id: 1},
+  {title: 'Ponderful Note.', body: 'This note is about being a Ponderful note.  It is a special note, this note.  Ponderful.', author_id: 0, notebook_id: 1},
   {title: 'Quonderful Note.', body: 'This note is about being a Quonderful note.  It is a special note, this note.  Quonderful.', author_id: 0, notebook_id: 0}
 
 ])
