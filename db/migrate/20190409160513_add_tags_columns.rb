2d8b40d200b703a9676151eb53239be16d3989c7
Jason Bourgoin <jason@jmbourgoin.com>
2019-04-22 12:04:29 -0400

    edit migration

--- db/migrate/20190409160513_add_tags_columns.rb
+++ db/migrate/20190409160513_add_tags_columns.rb
@@ -1,6 +1,5 @@
 class AddTagsColumns < ActiveRecord::Migration[5.2]
   def change
     add_column :tags, :author_id, :integer, null: false
-    add_column :tags, :name, :string, null: false
   end
 end
