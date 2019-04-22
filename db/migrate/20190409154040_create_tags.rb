35d24db6dd8ee4c2bd4879f9b7dcbdd868417d58
Jason Bourgoin <jason@jmbourgoin.com>
2019-04-22 16:54:32 -0400

    edit migration

--- db/migrate/20190409154040_create_tags.rb
+++ db/migrate/20190409154040_create_tags.rb
@@ -2,7 +2,7 @@ class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
       t.integer :author_id, null: false
-      t.string :name, :string, null: false
+      t.string :name, null: false
       t.timestamps
     end
     add_index :tags, :author_id, unique: true
